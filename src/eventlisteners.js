b77f3052711f6272a0576da9e3f7603359b31896
Adam Koch <adam@dogtired.org>
2020-01-31 16:29:05 -0600

    more event listeners; removed check for re-adding for now

--- src/eventlisteners.js
+++ src/eventlisteners.js
@@ -1,18 +1,22 @@
-if (!document.buddyeventlistenerswindow) {
+// if (!document.buddyeventlistenerswindow) {
 
+  jQuery.holdReady(true);
   document.buddyeventlistenerswindow = window;
-
+  
   let events = [];
-
+  let skipEvents = [
+    'mouseout', 'mousemove', 'mouseover', 'mousewheel', 'mousedown', 'mouseup', 'mouseenter', 'mouseleave',
+    'vmouseout', 'vmousemove', 'vmouseover',
+    'pointerover', 'pointermove', 'pointerout', 'pointerrawupdate', 'pointerdown', 'pointerenter', 'pointerleave',
+    'deviceorientationabsolute', 'scroll', 'wheel',
+    'focus', 'blur',
+    'keyup', 'keydown', 'keypress', 'input',
+    'scrollstart', 'scrollstop'
+  ];
+  
   for (attr in window) {
     if (attr.indexOf('on') == 0) {
-      if (![
-          'mouseout', 'mousemove', 'mouseover', 'mousewheel', 'mousedown', 'mouseup',
-          'pointerover', 'pointermove', 'pointerout', 'pointerrawupdate', 'pointerdown',
-          'deviceorientationabsolute', 'scroll', 'wheel',
-          'focus', 'blur',
-          'keyup', 'keydown', 'keypress', 'input'
-        ].includes(attr.substring(2))) {
+      if (!skipEvents.includes(attr.substring(2))) {
         events.push(attr);
         window.addEventListener(attr.substring(2), function (event) {
           console.log("window event", event.type);
@@ -20,6 +24,37 @@ if (!document.buddyeventlistenerswindow) {
       }
     }
   }
-
-  console.log('window events registered:', events.join(', '));
-}
+  
+  for (attr in document) {
+    if (attr.indexOf('on') == 0) {
+      if (!skipEvents.includes(attr.substring(2))) {
+        events.push(attr);
+        document.addEventListener(attr.substring(2), function (event) {
+          console.log("document event", event.type);
+        });
+      }
+    }
+  }
+  
+  for (attr in $.event.global) {
+    if (attr) {
+      if (!skipEvents.includes(attr)) {
+        events.push(attr);
+        $(document).on(attr, function (event) {
+          console.log("jquery event", event.type);
+        });
+      }
+    }
+  }
+  
+  
+  // ['pageshow', 'beforepageshow'].forEach(attr => {
+  //   events.push(attr);
+  //   $(document).on(attr, function (event) {
+  //     console.log("jquery event", event.type);
+  //   });
+  // })
+  
+  console.log('events registered:', events.join(', '));
+  // }
+  jQuery.holdReady(false);
