99d9929bb0478ecbe65a491d4015c6f402816267
Adam Koch <adam@dogtired.org>
2020-02-01 19:32:41 -0600

    saving logger - going to delete

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -11,7 +11,9 @@ function fakeConsole() {
   return {
     log: function () {},
     info: function () {},
-    debug: function () {}
+    debug: function () {},
+    warn: function () {},
+    error: function () {}
   };
 }
 
@@ -40,4 +42,51 @@ let isProduction = process.env.NODE_ENV === 'production';
 
 // module.exports = Level
 
-export default isProduction ? fakeConsole() : console;
+let getTime = () => {
+  let date = new Date();
+  let times = [date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds()].map((num) => {
+    if (num < 10)
+      return '0' + num;
+    return num;
+  })
+  let millis = date.getUTCMilliseconds().toString().padStart(3, '0');
+  return times.join(':') + '.' + millis;
+}
+
+const prompt = 'sb>';
+const Level = {
+  DEBUG: 1,
+  INFO: 2,
+  WARN: 3,
+}
+
+class Logger {
+  return {
+    debug: function (...args) {
+      if (this.level >= Level.DEBUG)
+        console.debug(prompt, getTime(), ...args);
+    },
+    log: function (...args) {
+      console.log(prompt, getTime(), ...args);
+    },
+    info: function (...args) {
+      if (this.level >= Level.INFO)
+        console.info(prompt, getTime(), ...args);
+    },
+    warn: function (...args) {
+      if (this.level >= Level.WARN)
+        console.warn(prompt, getTime(), ...args);
+    },
+    error: function (...args) {
+      console.error(prompt, getTime(), ...args);
+    },
+    level: (level) => {
+      this.level = level;
+      return level;
+    }
+  }
+}
+
+logger.level = 'debug';
+
+export default isProduction ? fakeConsole() : logger;
